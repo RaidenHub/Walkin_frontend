ed53026eaafba76ad9b0f55b2d2a30249b27d540
Prashant <74547535+Prashant-student@users.noreply.github.com>
2022-07-04 21:36:58 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1 class="text-5xl text-blue-700">Hello Typescript!</h1>
+  <Counter />
+  <p>
+    apps.
+  </p>
+</main>
